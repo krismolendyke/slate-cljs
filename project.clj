9fbeac42349957d6825c32869c1481ce024141d2
Kris <krismolendyke+github@gmail.com>
2013-12-26 23:02:56 -0500

    Add project description.

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject slate-cljs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Slate window management configuration."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
