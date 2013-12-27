06c759c673818c240defaaa556694e36f203b125
Kris <krismolendyke+github@gmail.com>
2013-12-26 20:36:56 -0500

    Add move function.

--- src/cljs/slate_cljs/slate.cljs
+++ src/cljs/slate_cljs/slate.cljs
@@ -24,9 +24,18 @@
   [win]
   (push win "left" "bar-resize:screenSizeX/2.5"))
 
+(defn move-center
+  [win]
+  (.doOperation win (.op js/S "move"
+                         (js-obj "x" "(screenSizeX / 2) - (windowSizeX / 2)"
+                                 "y" "screenOriginY"
+                                 "width" "windowSizeX"
+                                 "height" "windowSizeY"))))
+
 (.configAll js/S (js-obj "keyboardLayout" "dvorak"))
 
 (bind "r" (.op js/S "relaunch"))
 
 (bind-win "right" push-right)
 (bind-win "left" push-left)
+(bind-win "c" move-center)
