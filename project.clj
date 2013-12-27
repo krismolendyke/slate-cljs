f1ed4acc3cd0a090e540c0aaeb6a3425de0aabd1
Kris <krismolendyke+github@gmail.com>
2013-12-26 23:03:34 -0500

    Add project URL.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject slate-cljs "0.1.0-SNAPSHOT"
   :description "Slate window management configuration."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/jigish/slate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
