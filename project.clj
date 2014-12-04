207f8f9831b03d41a337eeeadac8f3a7b27ea404
Kris <krismolendyke+github@gmail.com>
2014-12-04 16:21:50 -0500

    UPGRAYEDD clojurescript and cljsbuild.

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2127"]]
-  :plugins [[lein-cljsbuild "1.0.1"]]
+                 [org.clojure/clojurescript "0.0-2371"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
   :cljsbuild {:builds
               [{
                 :source-paths ["src/cljs"]
