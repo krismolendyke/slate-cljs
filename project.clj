387fd3294c44c03dc8d9e6731d79fb8d221da653
Kris <krismolendyke+github@gmail.com>
2014-12-04 16:18:38 -0500

    UPGRAYEDD Clojure.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/jigish/slate"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2127"]]
   :plugins [[lein-cljsbuild "1.0.1"]]
   :cljsbuild {:builds
